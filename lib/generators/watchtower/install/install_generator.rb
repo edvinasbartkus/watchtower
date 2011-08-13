8c682babcd15333b1f8d27f30738fee1c39369fb
Edvinas Bartkus <edvinas@geeks.lt>
2011-08-14 01:55:19 +0300

    Static assets loaded from gem

--- lib/generators/watchtower/install/install_generator.rb
+++ lib/generators/watchtower/install/install_generator.rb
@@ -19,14 +19,6 @@ module Watchtower
       def copy_migrations
         migration_template "create_watched_exceptions.rb", "db/migrate/create_watched_exceptions.rb"
       end
-      
-      def manifest
-        root = File.join(File.dirname(__FILE__), "templates/public")
-        Dir.glob(File.join(root, "**", "*.*")).each do |f|
-          file = "public/"+f.gsub("#{root}/", "")
-          copy_file file, file  
-        end
-      end
     end
   end
 end
